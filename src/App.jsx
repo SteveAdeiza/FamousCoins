1e15ede1d14fbe1e4c5a55723063c3a1fea3e286
SteveAdeiza <105511911+SteveAdeiza@users.noreply.github.com>
2026-05-12 18:43:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import Admin from './pages/Admin'
-import Whitepaper from './pages/Whitepaper'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Whitepaper from './pages/Whitepaper';
 
 export default function App(){
-  return(
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/admin' element={<Admin />} />
-        <Route path='/whitepaper' element={<Whitepaper />} />
-      </Routes>
-    </BrowserRouter>
-  )
+ return <Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/dashboard' element={<Dashboard/>}/>
+  <Route path='/whitepaper' element={<Whitepaper/>}/>
+ </Routes>
 }
