efebab503d10067b02935d6bd4ed6a12305f858c
SteveAdeiza <105511911+SteveAdeiza@users.noreply.github.com>
2026-05-12 18:44:36 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,17 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "ADD_FIREBASE_KEY",
-  authDomain: "ADD_DOMAIN",
-  projectId: "ADD_PROJECT_ID",
-  storageBucket: "ADD_BUCKET",
-  messagingSenderId: "ADD_SENDER_ID",
-  appId: "ADD_APP_ID"
-}
+  apiKey: "AIzaSyBCep3LvTgjFUeRhgbcaGYObTWaNK7TWXQ",
+  authDomain: "famous-coins.firebaseapp.com",
+  projectId: "famous-coins",
+  storageBucket: "famous-coins.firebasestorage.app",
+  messagingSenderId: "961428237304",
+  appId: "1:961428237304:web:325afc7137bf4c5b56440d",
+  measurementId: "G-SERNPQF5M4"
+};
 
-export const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
