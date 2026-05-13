98794b3c1934d29d8e393cb1f9ff9bcfa5bf997f
SteveAdeiza <105511911+SteveAdeiza@users.noreply.github.com>
2026-05-13 19:33:21 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBCep3LvTgjFUeRhgbcaGYObTWaNK7TWXQ",
+  authDomain: "famous-coins.firebaseapp.com",
+  projectId: "famous-coins",
+  storageBucket: "famous-coins.firebasestorage.app",
+  messagingSenderId: "961428237304",
+  appId: "1:961428237304:web:325afc7137bf4c5b56440d",
+  measurementId: "G-SERNPQF5M4"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
